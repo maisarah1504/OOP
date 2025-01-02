ea219f0d4cc27c6ab5752898b5e71a5261eb207d
maisarah1504 <maisarahyusoff03@gmail.com>
2025-01-02 09:59:29 +0800

    maisarah

---
+++ src/oopproject/User.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oopproject;
+
+/**
+ *
+ * @author USER
+ */
+public class User {
+    private String name; 
+    private int phoneNumber; 
+    private String username; 
+    private int ic; 
+    private String email; 
+    private String address; 
+    
+    public User(){
+        
+    }
+}
