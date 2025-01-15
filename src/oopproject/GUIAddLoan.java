1df03f8043aa0ea992b111c559f4f3a2f9ae4fc3
ainna <ainna@DESKTOP-TVOE7OL>
2025-01-15 17:59:53 +0800

    ain

--- src/oopproject/GUIAddLoan.java
+++ src/oopproject/GUIAddLoan.java
@@ -9,12 +9,12 @@ package oopproject;
  *
  * @author ainna
  */
-public class GUIUserDashboard extends javax.swing.JFrame {
+public class GUIAddLoan extends javax.swing.JFrame {
 
     /**
      * Creates new form GUIUserDashboard
      */
-    public GUIUserDashboard() {
+    public GUIAddLoan() {
         initComponents();
     }
 
@@ -27,17 +27,30 @@ public class GUIUserDashboard extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        UserDashboard = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setBackground(new java.awt.Color(153, 0, 153));
+
+        UserDashboard.setFont(new java.awt.Font("Lucida Bright", 1, 18)); // NOI18N
+        UserDashboard.setForeground(new java.awt.Color(51, 0, 0));
+        UserDashboard.setText("User Dashboard");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(158, Short.MAX_VALUE)
+                .addComponent(UserDashboard)
+                .addGap(150, 150, 150))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(33, 33, 33)
+                .addComponent(UserDashboard)
+                .addContainerGap(280, Short.MAX_VALUE))
         );
 
         pack();
@@ -60,24 +73,26 @@ public class GUIUserDashboard extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(GUIUserDashboard.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(GUIAddLoan.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(GUIUserDashboard.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(GUIAddLoan.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(GUIUserDashboard.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(GUIAddLoan.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(GUIUserDashboard.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(GUIAddLoan.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new GUIUserDashboard().setVisible(true);
+                new GUIAddLoan().setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel UserDashboard;
     // End of variables declaration//GEN-END:variables
 }
